e8e7a5691c9a787ed8c4607acc3ff653df94871b
mahmoud alwakeel <mahmoudalwakeel200@gmail.com>
2022-06-19 12:50:02 +0200

    first commit

---
+++ drinks/drinks/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from operator import mod
+from rest_framework import serializers
+from .models import Drink
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ['id', 'name', 'description']
